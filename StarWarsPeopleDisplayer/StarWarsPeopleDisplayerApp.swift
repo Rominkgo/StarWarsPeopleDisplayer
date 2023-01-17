1981101674193233efe86fbbbfe4a949da7569b1
David Castaneda <davidehcastaneda@gmail.com>
2023-01-17 14:09:57 -0500

    Initial Commit

---
+++ StarWarsPeopleDisplayer/StarWarsPeopleDisplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsPeopleDisplayerApp.swift
+//  StarWarsPeopleDisplayer
+//
+//  Created by David Castaneda on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsPeopleDisplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
